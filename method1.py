f2cc002d793cd9c2cbffa9abfcd0150c7aaf5ff0
jadfsakwj <34261205+jadfsakwj@users.noreply.github.com>
2017-12-05 16:21:55 +0900

    Update method1.py
    fill?

--- method1.py
+++ method1.py
@@ -3,26 +3,26 @@ import random
 
 #はさみ、貯金箱、便座シート、コロコロ、Ziploc、ステッカー(猫)、Xmasカチューシャ、メダル
 data = [
-[0,0,0,2,3,0,0,0],
-[0,2,0,0,3,0,0,0],
-[0,0,0,0,0,0,0,0],
-[0,5,0,0,0,0,0,0],
-[0,0,0,5,0,0,0,0],
-[0,0,0,5,0,0,0,0],
-[0,2,0,0,3,0,0,0],
-[0,0,0,0,0,0,0,0],
-[0,0,3,2,0,0,0,0],
-[0,0,0,0,0,0,0,0],
-[0,0,0,5,0,0,0,0],
-[0,0,0,2,3,0,0,0],
-[0,3,0,0,0,2,0,0],
-[0,0,0,0,0,0,0,0],
+[0,0,0,0,1,0,4,0],
+[0,0,2,2,1,0,0,0],
+[0,0,0,0,5,0,0,0],
+[0,0,0,3,0,2,0,0],
 [0,0,0,0,0,5,0,0],
-[0,0,0,2,3,0,0,0],
+[4,1,0,0,0,0,0,0],
+[0,0,0,2,0,0,1,2],
+[0,0,0,0,0,2,0,3],
+[0,0,0,0,0,0,5,0],
+[0,4,0,1,0,0,0,0],
+[5,0,0,0,0,0,0,0],
+[0,0,3,2,0,0,0,0],
+[0,0,5,0,0,0,0,0],
+[1,0,0,0,4,0,0,0],
+[0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0],
 ]
 
 ans = [ [1,0],[1,0],[2,0],[2,0],[3,0],[3,0],[4,0],[4,0],[5,0],[5,0],[6,0],[6,0],[7,0],[7,0],[8,0],[8,0] ]
-name = [i for i in range(16)]
+people = [i for i in range(16)]
 
     
 def find(data):
@@ -36,40 +36,32 @@ def find(data):
     if pdsum[pdmax]<pdsum[i]:
       pdmax=i
   return pdmax
-       
+      
+    
 
-def select(pdmax):  
+def select(num):  
   weight = [0]*16
   sum = 0
   for i in range(16):
-     sum += data[i][pdmax]
+     sum += people[i][num]
   for i in range(16):
-     weight = data[i][pdmax]/sum
-        
+     weight = people[i][num]/sum
   a = random.choice(name, p=weight) 
-  ans[pdmax*2][1] = a
-  data[a] = [0]*8
-  people.remove(a)
+  b = random.choice(name, p=weight) 
+  while a == b:
+    b = random.choice(name, p=weight) 
+  people[a] = [0]*8
+  people[b] = [0]*8
+  return a,b
 
-  total = 0
-  for i in data:
-        for j in range(8):
-            total += i[j]
-  
-  if total != 0:  
-      b = a
-      while a == b:
-        b = random.choice(name, p=weight) 
-      ans[pdmax*2+1][1] = b
-      data[b] = [0]*8
-      people.remove(b)
-    
-for i in range(16):
-    data[i][pdmax] = 0
-
-
-
-  
+def fill(ans,people):
+       if people != []:
+            random.shuffle(people)
+            for i in ans:
+                if i[1] == 0:
+                i[1] = people[0]
+                people.remove(people[0])
+                    
   
      
   
