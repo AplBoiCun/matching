e5b685e97ed78ca93a5b388fe6194dcfb48939dc
Rintaro_h <aplboicundaze@gmail.com>
2017-12-05 17:16:55 +0900

    Update method1.py

--- method1.py
+++ method1.py
@@ -1,12 +1,12 @@
 # coding: utf-8
-import random
-    
+import numpy as np
+
 def find(data):
   pdsum = [0,0,0,0,0,0,0,0]
   for i in data:
     for j in range(8):
       pdsum[j] += i[j]
-    
+
   pdmax=0
   for i in range(8):
     if pdsum[pdmax]<pdsum[i]:
@@ -15,46 +15,51 @@ def find(data):
 
 def total(data):
     sum = 0
-    for i in data:	
-         for j in range(8):		
-             sum += i[j]	
+    for i in data:
+         for j in range(8):
+             sum += i[j]
     return sum
- 
 
-def select(pdmax,data):  		 
-    weight = [0]*16		   
-    sum = 0		  
-    for i in range(16):		   
-      sum += data[i][pdmax]		
-    for i in range(16):		  
-      weight = data[i][pdmax]/sum		
-         		
-    a = random.choice(name, p=weight) 		
-   ans[pdmax*2][1] = a		
-   data[a] = [0]*8		 
-   people.remove(a)	
- 		
-   if total(data) != 0:  		
-       b = a		
-       while a == b:		
-         b = random.choice(name, p=weight) 		
-       ans[pdmax*2+1][1] = b		
-       data[b] = [0]*8		
-       people.remove(b)		
-     		
- for i in range(16):		
-     data[i][pdmax] = 0		
-  
+
+def select(pdmax,data,people,ans):
+    weight = [0]*16
+    sum = 0
+    for i in range(16):
+      sum += data[i][pdmax]
+    for i in range(16):
+      weight[i] = data[i][pdmax]/sum
+
+    a = np.random.choice(people, p=weight)
+    ans[pdmax*2][1] = a
+    data[a] = [0]*8
+    people[a] = 0
+
+    if sum != data[a][pdmax]:
+        b = a
+        while a == b:
+            b = np.random.choice(people, p=weight)
+        ans[pdmax*2+1][1] = b
+        data[b] = [0]*8
+        people[b] = 0
+
+    for i in range(16):
+        data[i][pdmax] = 0
+
 
 def fill(ans,people):
-       if people != []:
-            random.shuffle(people)
+       if people != [0]*16:
+            np.random.shuffle(people)
+            people = [i for i in people if i != 0]
+            print(people)
+            print(ans)
+            a = 0
             for i in ans:
                 if i[1] == 0:
-                i[1] = people[0]
-                people.remove(people[0])
-                
+                    i[1] = people[a]
+                    a += 1
+
 def main():
+    np.random.seed(0)
     #はさみ、貯金箱、便座シート、コロコロ、Ziploc、ステッカー(猫)、Xmasカチューシャ、メダル
     data = [
     [0,0,0,0,1,0,4,0],
@@ -75,23 +80,13 @@ def main():
     [0,0,0,0,0,0,0,0],
     ]
     ans = [ [1,0],[1,0],[2,0],[2,0],[3,0],[3,0],[4,0],[4,0],[5,0],[5,0],[6,0],[6,0],[7,0],[7,0],[8,0],[8,0] ]
-    people = [i for i in range(16)]
-    
+    people = [i for i in range(1,17)]
+
     while total(data) != 0:
         pdmax = find(data)
-        select(pdmax)
+        select(pdmax,data,people,ans)
     fill(ans,people)
     print(ans)
 
-    
-    if __name__ == '__main__':
+if __name__ == '__main__':
     main()
-    
-                    
-  
-     
-  
-  
-
-
-
